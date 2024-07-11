18dcbd55e6e53d376c557f0ada1f2257a8cb8daa
drzazi <101451206+drzazi@users.noreply.github.com>
2024-07-11 15:57:52 +0800

    Update fail2ban.sh

--- fail2ban.sh
+++ fail2ban.sh
@@ -2,51 +2,43 @@
 
 if [ -f /etc/debian_version ]; then
     OS="Debian"
+    LOG_PATH="/var/log/auth.log"
 elif [ -f /etc/redhat-release ]; then
     OS="CentOS"
+    LOG_PATH="/var/log/secure"
 else
+    echo "Unsupported OS."
     exit 1
 fi
 
-# Debian
+# Get SSH port dynamically
+SSHD_PORT=$(sshd -T | grep "^port " | awk '{print $2}')
+
+# Install fail2ban
 if [ "$OS" = "Debian" ]; then
     apt-get install -y fail2ban
-    systemctl start fail2ban
-    systemctl enable fail2ban
-    sudo cp /etc/fail2ban/jail.{conf,local}
-    sudo bash -c 'echo "[DEFAULT]
-ignoreip = 127.0.0.1/8
-
-[sshd]
-enabled = true
-backend=systemd
-port = 22
-filter = sshd
-logpath = /var/log/auth.log
-bantime  = 1d
-findtime  = 5m
-maxretry = 3" > /etc/fail2ban/jail.local'
-    systemctl restart fail2ban
-    systemctl status fail2ban
-
-# CentOS
 elif [ "$OS" = "CentOS" ]; then
     yum install fail2ban -y
-    systemctl start fail2ban
-    systemctl enable fail2ban
-    sudo cp /etc/fail2ban/jail.{conf,local}
-    sudo bash -c 'echo "[DEFAULT]
+fi
+
+# Configure fail2ban
+cat << EOF > /etc/fail2ban/jail.local
+[DEFAULT]
 ignoreip = 127.0.0.1/8
 
 [sshd]
 enabled = true
-backend=systemd
-port = 22
+backend = systemd
+port = $SSHD_PORT
 filter = sshd
-logpath = /var/log/secure
-bantime  = 1d
-findtime  = 5m
-maxretry = 3" > /etc/fail2ban/jail.local'
-    systemctl restart fail2ban
-    systemctl status fail2ban
-fi
+logpath = $LOG_PATH
+bantime = 1d
+findtime = 5m
+maxretry = 3
+EOF
+
+# Start and enable fail2ban
+systemctl start fail2ban
+systemctl enable fail2ban
+systemctl restart fail2ban
+systemctl status fail2ban
